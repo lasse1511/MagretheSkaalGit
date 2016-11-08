20fd538df3c9f480609052c183afe913a4f1332e
lasse1511 <lassebonner@hotmail.com>
2016-11-08 18:27:53 +0100

    Viselse af navn i playscreen og virkning af pass og right btn

--- app/src/main/java/com/example/lasse/magretheskaal/PlayScreen.java
+++ app/src/main/java/com/example/lasse/magretheskaal/PlayScreen.java
@@ -1,7 +1,6 @@
 package com.example.lasse.magretheskaal;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -27,27 +26,48 @@ public class PlayScreen extends AppCompatActivity {
         builder.setTitle("Round Finished");
         builder.setMessage("Next round");
         final AlertDialog alert = builder.create();
-        TextView names = (TextView) findViewById(R.id.text_namesPlay);
-        Button right = (Button) findViewById(R.id.BTN_right);
+        final TextView names = (TextView) findViewById(R.id.text_namesPlay);
+        final Button right = (Button) findViewById(R.id.BTN_right);
+        final Button pass = (Button) findViewById(R.id.BTN_pass);
 
         logic.NamesOrg = getIntent().getExtras().getStringArrayList("NamesOrg");
-         logic.NamesEdit = getIntent().getExtras().getStringArrayList("NamesEdit");
+        logic.NamesEdit = getIntent().getExtras().getStringArrayList("NamesEdit");
         logic.RoundTime =getIntent().getExtras().getInt("RoundTime");
         logic.RoundType = getIntent().getExtras().getStringArrayList("RoundType");
         logic.Team1Score = getIntent().getExtras().getInt("Team1Score");
         logic.Team2Score = getIntent().getExtras().getInt("Team2Score");
 
 
+        index =  rand.nextInt(logic.NamesEdit.size());
+        names.setText(logic.NamesEdit.get(index));
+
+
 
         right.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (logic.NamesEdit.size() > 0) {
+                    logic.removeName(index);
+                    index = rand.nextInt(logic.NamesEdit.size());
+                    names.setText(logic.NamesEdit.get(index));
+                }
 
+                    //startActivity(i);
             }
         });
 
-        final Intent i = new Intent(PlayScreen.this, BetweenScreen.class);
 
+        pass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                index = rand.nextInt(logic.NamesEdit.size());
+                names.setText(logic.NamesEdit.get(index));
+            }
+        });
+
+
+
+        final Intent i = new Intent(PlayScreen.this, BetweenScreen.class);
         final TextView mTextField = (TextView) findViewById(R.id.Countdowntimer);
         Integer rt = (logic.getRoundTime()*1000)+1000;
         new CountDownTimer(rt, 1000) {
@@ -57,10 +77,9 @@ public class PlayScreen extends AppCompatActivity {
                 //here you can have your logic to set text to edittext
                if((millisUntilFinished/1000)==1)
                 {
-
                     alert.show();
-
-
+                    right.setActivated(false);
+                    pass.setActivated(false);
                     i.putExtra("NamesOrg", logic.NamesOrg);
                     i.putExtra("NamesEdit", logic.NamesEdit);
                     i.putStringArrayListExtra("RoundType", logic.RoundType);
@@ -82,19 +101,6 @@ public class PlayScreen extends AppCompatActivity {
 
         }.start();
 
-        /*index = rand.nextInt(logic.NamesEdit.size());
-        names.setText(logic.NamesEdit.get(index));
-*/
-
         }
-
-
-
-
-
-    public void setLogic(LogicLayer logic_)
-    {
-        logic = logic_;
-    }
 
 }
